e353890becffb9bc2c7ea49c9fa5b200e36c023a
Emre Iskın <gigachadbaldi@gmail.com>
2025-11-15 19:54:30 +0300

    Add VSCode extension setup wizard implementation

---
+++ extensions/setup-wizard/src/extension.ts
@@ -0,0 +1,14 @@
+// The entry point of the VS Code Extension Setup Wizard
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    // This method is called when your extension is activated
+    console.log('Congratulations, your extension "setup-wizard" is now active!');
+
+    // Add your commands and functionality here
+}
+
+export function deactivate() {
+    // This method is called when your extension is deactivated
+}
